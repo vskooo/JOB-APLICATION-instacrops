5442c3826c41d5989f98cc72d1fee7a9f876e370
asko <faviortega.b@gmail.com>
2022-11-14 22:24:08 -0300

    DATA TO FRONT

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AskoService } from '../asko.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   hola: string = 'WENA PERROOOO';
-  constructor() {}
+  constructor(
+    private service: AskoService
+  ) {}
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  this.traerDatos();
+}
+async traerDatos () {
+  let response = await this.service.TraerTodos();
+  console.log(response);
+  
 
+}
 }
